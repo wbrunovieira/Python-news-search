7b3c77a40189b4660fff6317af7fb865e06464f1
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-23 17:21:33 -0300

    improve the formats the news

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ from bs4 import BeautifulSoup
 import feedparser
 from lxml import etree
 
-def parse_html_with_lxml(content):
-    # Usando lxml como parser do BeautifulSoup para HTML
-    soup = BeautifulSoup(content, 'lxml')
-    # Adicione o código para extrair as informações do 'soup'
-    # ...
+def format_news(title, link, description):
+    return f"Título: {title}\nLink: {link}\nDescrição: {description}\n"
 
+def parse_html_with_lxml(url, content):
+    soup = BeautifulSoup(content, 'lxml')
+
+    found_news = False
+    for news_item in soup.find_all(['h1', 'h2', 'h3']):
+        link = news_item.find('a')
+        if link and link.get('href'):
+            title = news_item.get_text(strip=True)
+            news_link = link.get('href')
+
+            # Verifica se o link é relativo ou absoluto
+            if not news_link.startswith('http'):
+                news_link = url + news_link
+
+            print(f"Título: {title}\nLink: {news_link}\n")
+            found_news = True
+
+    if not found_news:
+        print(f"Nenhuma notícia encontrada em {url}")
 def parse_rss_with_lxml(url):
     response = requests.get(url)
     tree = etree.fromstring(response.content)
-    # Adicione o código para extrair as informações do feed RSS
-    # Exemplo: iterar pelos elementos do feed
     for item in tree.findall('channel/item'):
         title = item.find('title').text
         link = item.find('link').text
         description = item.find('description').text
-        print(title, link, description)
+        print(format_news(title, link, description))
 
 def main():
     # Lista de URLs de páginas HTML
@@ -34,12 +48,21 @@ def main():
     
     # Loop para processar cada URL HTML
     for url in html_urls:
-        response = requests.get(url)
-        parse_html_with_lxml(response.content)
+          try:
+            response = requests.get(url, timeout=5)
+            print(f"Notícias de {url}:\n")
+            parse_html_with_lxml(url, response.content)
+          except Exception as e:
+            print(f"Erro ao processar {url}: {e}")
+        
 
     # URL do feed RSS
     rss_url = "https://www.tabnews.com.br/recentes/rss"
-    parse_rss_with_lxml(rss_url)
+    try:
+        print(f"Notícias do RSS {rss_url}:\n")
+        parse_rss_with_lxml(rss_url)
+    except Exception as e:
+        print(f"Erro ao processar RSS {rss_url}: {e}")
 
 if __name__ == "__main__":
     main()
